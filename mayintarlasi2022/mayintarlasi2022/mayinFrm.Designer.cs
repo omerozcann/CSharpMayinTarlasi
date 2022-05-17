55c5715cd307d76061536bcda7fabf18edfc1636
omerozcann <oozcan@selcuk.edu.tr>
2022-05-17 17:37:28 +0300

    .

---
+++ mayintarlasi2022/mayintarlasi2022/mayinFrm.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace mayintarlasi2022
+{
+    partial class mayinFrm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.yenioyunbtn = new System.Windows.Forms.Button();
+            this.mesajLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 61);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(353, 297);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Oyun Alanı";
+            // 
+            // yenioyunbtn
+            // 
+            this.yenioyunbtn.Location = new System.Drawing.Point(229, 22);
+            this.yenioyunbtn.Name = "yenioyunbtn";
+            this.yenioyunbtn.Size = new System.Drawing.Size(117, 33);
+            this.yenioyunbtn.TabIndex = 1;
+            this.yenioyunbtn.Text = "Yeni Oyun";
+            this.yenioyunbtn.UseVisualStyleBackColor = true;
+            this.yenioyunbtn.Click += new System.EventHandler(this.yenioyunbtn_Click);
+            // 
+            // mesajLbl
+            // 
+            this.mesajLbl.AutoSize = true;
+            this.mesajLbl.Location = new System.Drawing.Point(9, 30);
+            this.mesajLbl.Name = "mesajLbl";
+            this.mesajLbl.Size = new System.Drawing.Size(0, 17);
+            this.mesajLbl.TabIndex = 2;
+            // 
+            // mayinFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 370);
+            this.Controls.Add(this.mesajLbl);
+            this.Controls.Add(this.yenioyunbtn);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "mayinFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mayın Tarlası";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button yenioyunbtn;
+        private System.Windows.Forms.Label mesajLbl;
+    }
+}
+
---
+++ mayintarlasi2022/mayintarlasi2022/mayinFrm.cs
@@ -0,0 +1,144 @@
+﻿/*
+ Öğr.Gör.Ömer ÖZCAN
+oozcan@selcuk.edu.tr
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mayintarlasi2022
+{
+    public partial class mayinFrm : Form
+    {
+        int[,] mayin = new int[10, 10];
+        Button[,] btn = new Button[10, 10];
+        int skor = 0;
+        public mayinFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void yenioyunbtn_Click(object sender, EventArgs e)
+        {
+            skor = 0;
+            groupBox1.Controls.Clear();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    btn[i, j] = new Button();
+                    btn[i, j].Width = 20;
+                    btn[i, j].Height = 20;
+                    btn[i, j].Name = i + "-" + j;
+                    btn[i, j].Top = i * 20+30;
+                    btn[i, j].Left = j * 20+30;
+                    btn[i, j].Parent = groupBox1;
+                    btn[i, j].Click += oyunbtn_Click;
+                    mayin[i, j] = 0;
+                    btn[i, j].BackgroundImage=null;
+                }
+            }
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int x = r.Next(10);
+                int y = r.Next(10);
+                mayin[x, y] = 1;
+            }
+            mesajLbl.Text = "Oyun Başladı";
+        }
+
+        private void oyunbtn_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            String[] dizi = b.Name.Split('-');
+            int x = int.Parse(dizi[0]);
+            int y = int.Parse(dizi[1]);
+            if(mayin[x,y]==1)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (mayin[i,j]==1)
+                        {
+                            btn[i, j].BackgroundImage = Image.FromFile("mayin.png");
+                            btn[i, j].BackgroundImageLayout = ImageLayout.Stretch;
+                        }
+                    }
+                }
+                SoundPlayer sp = new SoundPlayer();
+                sp.SoundLocation = "bigboom.wav";
+                sp.Play();
+                mesajLbl.Text = "Kaybettiniz";
+            }
+            else
+            {
+                int mayinSayi = 0;
+                if(x<9)
+                {
+                    if(mayin[x+1,y]==1)
+                    {
+                        mayinSayi++;
+                    }
+                }
+                if (x < 9&&y<9)
+                {
+                    if (mayin[x + 1, y+1] == 1)
+                    {
+                        mayinSayi++;
+                    }
+                }
+                if (y < 9)
+                {
+                    if (mayin[x, y+1] == 1)
+                    {
+                        mayinSayi++;
+                    }
+                }
+                if (x >0&&y<9)
+                {
+                    if (mayin[x - 1, y+1] == 1)
+                    {
+                        mayinSayi++;
+                    }
+                }
+                if (x>0)
+                {
+                    if (mayin[x - 1, y] == 1)
+                    {
+                        mayinSayi++;
+                    }
+                }
+                if (x >0&&y>0)
+                {
+                    if (mayin[x - 1, y-1] == 1)
+                    {
+                        mayinSayi++;
+                    }
+                }
+                if (y>0)
+                {
+                    if (mayin[x, y-1] == 1)
+                    {
+                        mayinSayi++;
+                    }
+                }
+                btn[x, y].Text = mayinSayi.ToString() ;
+                skor++;
+                mesajLbl.Text = "Skor:" + skor;
+                if(skor==90)
+                {
+                    mesajLbl.Text = "Kazandınız";
+                }
+            }
+        }
+    }
+}
